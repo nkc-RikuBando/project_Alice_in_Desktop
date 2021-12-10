675bd47d6104034398457c43c0f56ed752a259a2
GTA20_Kaede <nkc-kaede>
2021-12-10 11:56:34 +0900

    add:開く箱のゲージ完成

--- project_Alice_in_Desktop/Assets/Scripts/KeyAndDoor/Gimmicks/Box.cs
+++ project_Alice_in_Desktop/Assets/Scripts/KeyAndDoor/Gimmicks/Box.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Connector.Inputer;
+using GameSystem;
 
 namespace Gimmicks
 {
     public class Box : MonoBehaviour
     {
-        private GameObject player; // プレイヤーを保存
-        private ITestKey _ITestKey;
+        [SerializeField] private GameObject player; // プレイヤーを保存
+        private ITestKey _ITestKey; // 入力インターフェースを取得
         [SerializeField] private GameObject hideKey; // 鍵を取得
-        private bool stayFlg;
-        private float inputTime = 0;
-        //private bool hitFlg = false;
+        private bool stayFlg = false;
+        //private float inputTime = default;
 
         public bool PlHitFlg
         {
@@ -23,50 +23,43 @@ namespace Gimmicks
 
         void Start()
         {
-            player = GameObject.Find("PlayerTest"); // プレイヤーの取得
-            _ITestKey = GetComponent<ITestKey>();
-            hideKey.SetActive(false);
-            stayFlg = false;
+            _ITestKey = GetComponent<ITestKey>(); // 入力インターフェースを取得
+            hideKey.SetActive(false); // 鍵を非表示
         }
 
         void Update()
         {
-            if(StayInput())
+            if (StayInput())
             {
-                inputTime += 1f * Time.deltaTime;
-                if(inputTime >= 3)
+                //inputTime += 1f * Time.deltaTime;
+                if (/*inputTime >= 0.8f*/WaitTimeUI.gaugeMaxFlg == true)
                 {
+                    WaitTimeUI.gaugeMaxFlg = false;
                     KeyApp();
-                    hideKey.transform.parent = null;
+                    hideKey.transform.parent = null; // 鍵を子オブジェクトから外す
                     Destroy(gameObject);
                 }
             }
-            else
-            {
-                inputTime = default; // キー入力時間を０にする
-            }
+            //else
+            //    inputTime = default; // キー入力時間を０にする
         }
     
         private void OnCollisionEnter2D(Collision2D collision)
         {
             // プレイヤーが入って来たら
             if (collision.gameObject == player)
-            {
                 stayFlg = true; // 滞在中
-            }
         }
 
         private void OnCollisionExit2D(Collision2D collision)
         {
             // プレイヤーが出て行ったら
             if (collision.gameObject == player)
-            {
                 stayFlg = false; // 滞在してない
-            }
         }
 
         /// <summary>
-        /// プレイヤーが触れている、かつ、Qキーを長押し
+        /// プレイヤーが触れている、かつ、キーを長押し
         /// </summary>
         /// <returns></returns>
         bool StayInput()
@@ -77,7 +70,7 @@ namespace Gimmicks
         /// <summary>
         /// 鍵が出現する
         /// </summary>
-        public void KeyApp() // 箱(自分が壊れる)
+        public void KeyApp()
         {
             hideKey.SetActive(true); //壊れたら鍵が出現
         }
