3c140bd8bd38676bf471e513576bf675f97a4687
GTA20MiwaYuga <ym2054@mailg.denpa.ac.jp>
2021-12-13 19:20:10 +0900

    add カーソル変更 developでやりました 反省してま～す

--- project_Alice_in_Desktop/Assets/Scripts/Window/CursorChange.cs
+++ project_Alice_in_Desktop/Assets/Scripts/Window/CursorChange.cs
@@ -1,16 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class CursorChange : MonoBehaviour
 {
     [SerializeField] private Texture2D cursolTexture;
 
-    void Update()
+    public Texture2D GetTexture()
     {
-
+        return cursolTexture;
     }
-
 
 }
