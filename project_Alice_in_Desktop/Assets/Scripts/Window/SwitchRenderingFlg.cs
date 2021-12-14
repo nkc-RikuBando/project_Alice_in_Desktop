af0363418d7bd21c3fda057edc0bcc3d54a44624
GTA20MiwaYuga <ym2054@mailg.denpa.ac.jp>
2021-12-14 19:19:29 +0900

    add 埋まりバグ修正

--- project_Alice_in_Desktop/Assets/Scripts/Window/SwitchRenderingFlg.cs
+++ project_Alice_in_Desktop/Assets/Scripts/Window/SwitchRenderingFlg.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class SwitchRenderingFlg : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D col)
     {
         IRenderingFlgSettable _renderingFlgSettable = col.gameObject.GetComponent<IRenderingFlgSettable>();
