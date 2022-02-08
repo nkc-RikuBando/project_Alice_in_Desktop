0be9e67e4b13c2fccdbaf98411cfbb9cdd8645ab
GTA20MiwaYuga <ym2054@mailg.denpa.ac.jp>
2022-02-08 15:25:24 +0900

    add チュートリアルのカーソル出現

---
+++ project_Alice_in_Desktop/Assets/Scripts/Tutorial/CursolAppear.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Window;
+
+
+public class CursolAppear : MonoBehaviour,IWindowTouch
+{
+    [SerializeField] private GameObject tutorialObj;
+    private GameObject obj;
+    [SerializeField] private Vector3 pos;
+    [SerializeField] private float appearTime;
+    private bool appearFlg = true;
+    void Start()
+    {
+        StartCoroutine(Appear());
+    }
+
+    public void DeleteCursol()
+    {
+        appearFlg = false;
+        Destroy(obj);
+    }
+
+    IEnumerator Appear()
+    {
+        yield return new WaitForSeconds(appearTime);
+        if (appearFlg)
+        {
+            obj = Instantiate(tutorialObj);
+            obj.transform.position = pos;
+        }
+    }
+
+    public void WindowTouchAction()
+    {
+        if (appearFlg == true)
+        {
+            DeleteCursol();
+        }
+    }
+}
