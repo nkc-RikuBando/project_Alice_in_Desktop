1578914566ea502c7dc2f6a5edea26db4835ca96
GTA20bando <hisui111516@ezweb.ne.jp>
2022-02-13 17:14:22 +0900

    add 一方通行

---
+++ project_Alice_in_Desktop/Assets/Scripts/Graphic/FlipOneWayWall.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipOneWayWall : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player.PlayerCollisionDetection detection = collision.GetComponent<Player.PlayerCollisionDetection>();
+        if (detection != null) return; 
+        animator.SetTrigger("Flip");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //animator.SetTrigger("Flip");
+    }
+}
