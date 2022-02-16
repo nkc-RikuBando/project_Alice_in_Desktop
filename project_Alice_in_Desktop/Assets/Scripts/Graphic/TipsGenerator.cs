67db5e3d78c9256f82714448cb789183ec604e32
GTA20_Kaede <nkc-kaede>
2022-02-16 14:07:46 +0900

    add:ファイル

---
+++ project_Alice_in_Desktop/Assets/Scripts/Graphic/TipsGenerator.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+public class TipsGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject tipsObj;
+    private GameObject obj;
+    private bool generateFlg = true;
+    private bool enterFlg = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!generateFlg) return;
+        var player = collision.GetComponent<PlayerSet>();
+
+        // PlayerÇÃèÍçá
+        if (player != null)
+        {
+            // ìñÇΩÇ¡ÇΩéûèàóù
+            obj = Instantiate(tipsObj);
+            obj.transform.position = transform.position;
+            generateFlg = false;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!enterFlg) return;
+        var player = collision.GetComponent<PlayerSet>();
+
+        // PlayerÇÃèÍçá
+        if (player != null)
+        {
+            obj.GetComponent<Animator>().SetTrigger("PopUp");
+            enterFlg = false;
+        }
+    }
+}
